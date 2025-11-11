670abbae52e01c95020498a3590fc05d7c16d460
Santhosh Janardhanan <santhoshj@gmail.com>
2025-11-10 22:13:55 -0500

    more housekeeping

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,20 +2,20 @@
 <html lang="en">
 <head>
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7LVCT1XS20"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{env('GOOGLE_ANALYTICS_ID')}}"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-7LVCT1XS20');
+    gtag('config', '{{env("GOOGLE_ANALYTICS_ID")}}');
     </script>
     <script type="text/javascript" async>
         (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "pwm9zju7wt");
+        })(window, document, "clarity", "script", "{{env('MICROSOFT_CLARITY_ID')}}");
     </script>
 
     @php($recaptchaSiteKey = config('services.recaptcha.site_key'))
